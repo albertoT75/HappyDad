f50ce863008a45e1267150cbceefb159b8a025e4
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-06-20 12:40:24 +0200

    trying to serve

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -14,6 +14,72 @@ ActiveRecord::Schema.define(version: 2020_06_19_095624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "reservation_id"
+    t.text "content"
+    t.integer "rating"
+    t.index ["reservation_id"], name: "index_comments_on_reservation_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "description"
+    t.string "photo"
+  end
+
+  create_table "kids", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "first_name"
+    t.string "hair_color"
+    t.string "haircut"
+    t.string "eyes_color"
+    t.boolean "glasses"
+    t.string "up_color"
+    t.string "bottom_color"
+    t.index ["user_id"], name: "index_kids_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "kid_id"
+    t.bigint "game_id"
+    t.string "status"
+    t.integer "green_stars"
+    t.integer "yellow_stars"
+    t.integer "red_stars"
+    t.index ["game_id"], name: "index_reservations_on_game_id"
+    t.index ["kid_id"], name: "index_reservations_on_kid_id"
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "first_prize"
+    t.string "second_prize"
+    t.string "third_prize"
+    t.string "fourth_prize"
+    t.string "fifth_prize"
+    t.string "name"
+    t.index ["user_id"], name: "index_rewards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +89,14 @@ ActiveRecord::Schema.define(version: 2020_06_19_095624) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "reservations"
+  add_foreign_key "kids", "users"
+  add_foreign_key "reservations", "games"
+  add_foreign_key "reservations", "kids"
+  add_foreign_key "rewards", "users"
 end
